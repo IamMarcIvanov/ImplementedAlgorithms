6af80a9d35ad92675b144a9eaca32f6aea83f662
IamMarcIvanov <45270390+IamMarcIvanov@users.noreply.github.com>
2020-08-06 19:10:15 +0530

    Correct
    but not the one given in the text

--- randomized_select.py
+++ randomized_select.py
@@ -1,4 +1,4 @@
-def partition(A, start, end):
+def partition(A, start, end, ind):
     pivot = A[end]
     i = start - 1
     for j in range(start, end):
@@ -6,28 +6,31 @@ def partition(A, start, end):
             i += 1
             A[i], A[j] = A[j], A[i]
     A[i + 1], A[end] = A[end], A[i + 1]
+    if i + 1 == ind:
+        print(A[i + 1])
     return i + 1
 
-def randomized_partition(A, start, end):
+def randomized_partition(A, start, end, ind):
     import random
-    i = random.randrange(start, end, 1)
-    A[i], A[end] = A[end], A[i]
-    return partition(A, start, end)
-
-def randomized_select(A, start, end, ind):
-    if start <= end:
-        return A[start]
-    q = randomized_partition(A, start, end)
-    k = q - start + 1
-    if ind == k:
-        return A[ind - 1]
-    if ind < k:
-        randomized_select(A, start, k - 2, ind)
+    if start == end:
+        i = start
     else:
-        randomized_select(A, k, end, ind - k)
+        i = random.randrange(start, end, 1)
+    A[i], A[end] = A[end], A[i]
+    return partition(A, start, end, ind)
+
+def quicksort(A, start, end, ind):
+    if start <= end:
+        q = randomized_partition(A, start, end, ind)
+        if q == ind:
+            return A[q]
+        if ind > q:
+            quicksort(A, q + 1, end, ind)
+        else:
+            quicksort(A, start, q - 1, ind)
 
 import random
-arr = [random.randrange(-1000, 1000, 1) for _ in range(20)]
+arr = [random.randrange(-1000, 1000, 1) for _ in range(10)]
+print(arr)
 print(sorted(arr))
-print(randomized_select(arr, 0, 19, 5))
-#print(randomized_select([13, 5, 7], 0, 2, 2))
+quicksort(arr, 0, 9, 4)
