e3220d34d3cb6319680ced7ec10633096119869c
Sharanya Ranka <sharanya.ranka@gmail.com>
2020-08-11 18:17:35 +0530

    Changed p,q in randomised_partition() to start,end+1. Added random testcases at the end

--- quicksort.py
+++ quicksort.py
@@ -1,3 +1,5 @@
+import random
+
 def partition(A, start, end):
     pivot = A[end]
     i = start - 1
@@ -9,8 +11,8 @@ def partition(A, start, end):
     return i + 1
 
 def randomized_partition(A, start, end):
-    import random
-    i = random.randrange(p, q, 1)
+
+    i = random.randrange(start, end+1, 1)
     A[i], A[end] = A[end], A[i]
     return partition(A, start, end)
 
@@ -19,5 +21,11 @@ def quicksort(A, start, end):
         q = randomized_partition(A, start, end)
         quicksort(A, start, q - 1)
         quicksort(A, q + 1, end)
+
+
+list_to_sort=random.choices(range(1,int(1e5)),k=25)
+print("unsorted\n",list_to_sort)
+quicksort(list_to_sort,0,len(list_to_sort)-1)
+print("sorted\n",list_to_sort)
 
 
